1c26e3d9395caf147397317912b8405e26aef6cd
RAJA SETHI <55877197+RAJASETHI@users.noreply.github.com>
2022-05-26 09:11:20 +0530

    Time: 3 ms (54.92%), Space: 6 MB (48.74%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -4,6 +4,11 @@ public:
         int cnt=0;
         while(n)
         {
+            if(n&(n+1)==0)
+            {
+                cnt+=log2(n)+1;
+                return cnt;
+            }
             cnt+=n&1;
             n>>=1;
         }
