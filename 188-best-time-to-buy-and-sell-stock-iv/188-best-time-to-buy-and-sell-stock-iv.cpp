652938e646e052f274259ca7f0021627dfe4a045
RAJA SETHI <55877197+RAJASETHI@users.noreply.github.com>
2022-05-31 14:46:23 +0530

    Time: 15 ms (56.41%), Space: 12.7 MB (35.50%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,25 +1,65 @@
 class Solution {
 public:
-    int f(int idx,int buy,vector<int>&prices,int k,vector<vector<vector<int>>>&dp)
+    // int f(int idx,int buy,vector<int>&prices,int k,vector<vector<vector<int>>>&dp)
+    // {
+    //     if(idx==prices.size() || k==0)return 0;
+    //     int profit=0;
+    //     if(dp[idx][k][buy]!=-1)return dp[idx][k][buy];
+    //     if(buy)
+    //     {
+    //         profit=-prices[idx]+f(idx+1,0,prices,k,dp);
+    //         profit=max(profit,f(idx+1,1,prices,k,dp));
+    //     }
+    //     else
+    //     {
+    //         profit=prices[idx]+f(idx+1,1,prices,k-1,dp);
+    //         profit=max(profit,f(idx+1,0,prices,k,dp));
+    //     }
+    //     return dp[idx][k][buy]=profit;
+    // }
+    int f(int idx,int trns,vector<int>&prices,int k)
     {
-        if(idx==prices.size() || k==0)return 0;
+        if(idx==prices.size() || trns==2*k)return 0;
         int profit=0;
-        if(dp[idx][k][buy]!=-1)return dp[idx][k][buy];
-        if(buy)
+        if(trns%2==0)
         {
-            profit=-prices[idx]+f(idx+1,0,prices,k,dp);
-            profit=max(profit,f(idx+1,1,prices,k,dp));
+            profit=-prices[idx]+f(idx+1,trns+1,prices,k);
+            profit=max(profit,f(idx+1,trns,prices,k));
         }
         else
         {
-            profit=prices[idx]+f(idx+1,1,prices,k-1,dp);
-            profit=max(profit,f(idx+1,0,prices,k,dp));
+            profit=prices[idx]+f(idx+1,trns+1,prices,k);
+            profit=max(profit,f(idx+1,trns,prices,k));
         }
-        return dp[idx][k][buy]=profit;
+        return profit;
     }
     int maxProfit(int k, vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(k+1,vector<int>(2,-1)));
-        return f(0,1,prices,k,dp);
+        // Approach 1:
+        // vector<vector<vector<int>>>dp(n,vector<vector<int>>(k+1,vector<int>(2,-1)));
+        // return f(0,1,prices,k,dp);
+       // Approach 2:
+        // return f(0,0,prices,k);
+        vector<vector<int>>dp(n+1,vector<int>(2*k+1,0));
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=2*k-1;j>=0;j--)
+            {
+                int profit=0;
+                if(j%2==0)
+                {
+                    profit=-prices[i]+dp[i+1][j+1];
+                    profit=max(profit,dp[i+1][j]);
+                }
+                else
+                {
+                     profit=prices[i]+dp[i+1][j+1];
+                    profit=max(profit,dp[i+1][j]);
+                }
+                dp[i][j]=profit;
+            }
+        }
+        return dp[0][0];
     }
+    
 };
