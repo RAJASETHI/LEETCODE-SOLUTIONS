6be319f911b96955b88b1cc8988ceda184a031bf
RAJA SETHI <55877197+RAJASETHI@users.noreply.github.com>
2022-06-01 12:26:35 +0530

    Time: 8 ms (21.26%), Space: 8.6 MB (38.06%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -1,7 +1,18 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        partial_sum(nums.begin(),nums.end(),nums.begin());
-        return nums;
+        vector<int>res;
+        for(auto i:nums)
+        {
+            if(!res.size())
+            {
+                res.push_back(i);
+            }
+            else
+            {
+                res.push_back(res[res.size()-1]+i);
+            }
+        }
+        return res;
     }
 };
