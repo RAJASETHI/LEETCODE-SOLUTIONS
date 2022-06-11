a3bd6c06ce7f418aa8ef082503047ba3d9b0c75b
RAJA SETHI <55877197+RAJASETHI@users.noreply.github.com>
2022-06-11 10:02:29 +0530

    Time: 33 ms (27.82%), Space: 8.5 MB (47.21%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -2,6 +2,20 @@ class Solution {
 public:
     
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n=piles.size();
+        int dp[n+2][n+2];
+        memset(dp,0,sizeof(dp));
+        for(int sz=1;sz<=n;sz++)
+        {
+            for(int i=0,j=sz-1;j<n;i++,j++)
+            {
+                if((j+i+n)%2==1)
+                    dp[i+1][j+1]=max(piles[i]+dp[i+2][j+1],piles[j]+dp[i+1][j]);
+                else
+                    dp[i+1][j+1]=max(-piles[i]+dp[i+2][j+1],-piles[j]+dp[i+1][j]);
+                
+            }
+        }
+        return dp[1][n]>0;
     }
 };
