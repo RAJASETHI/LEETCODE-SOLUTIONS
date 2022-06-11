9b93b9fd630a0ea0f62eea3ecd42b1f283957927
RAJA SETHI <55877197+RAJASETHI@users.noreply.github.com>
2022-06-11 09:33:51 +0530

    Time: 798 ms (8.12%), Space: 31.1 MB (23.98%) - LeetHub

---
+++ 887-super-egg-drop/887-super-egg-drop.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int superEggDrop(int k, int n) {
+        vector<vector<int>>dp(k+2,vector<int>(n+2,0));
+        for(int i=0;i<=k;i++)
+        {
+            for(int j=0;j<=n;j++)
+            {
+                if(!i || !j)dp[i][j]=0;
+                else if(j==1)dp[i][j]=1;
+                else if(i==1)dp[i][j]=j;
+                else
+                {
+                    int mn=100000,l=0,r=j;
+                    while(l<=r)
+                    {
+                        int mid=l+(r-l)/2;
+                        int left=dp[i-1][mid-1];
+                        int right=dp[i][j-mid];
+                        int temp=max(left,right);
+                        if(left<right)l=mid+1;
+                        else r=mid-1;
+                        mn=min(mn,temp);
+                    }
+                    dp[i][j]=mn+1;
+                }
+            }
+        }
+        return dp[k][n];
+    }
+};
