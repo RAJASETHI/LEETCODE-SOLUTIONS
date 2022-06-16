04af78738600b43fe7d4b0e441c165cbfa584736
RAJA SETHI <55877197+RAJASETHI@users.noreply.github.com>
2022-06-16 15:12:39 +0530

    Time: 45 ms (94.17%), Space: 14.8 MB (94.18%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>>dd={{-1,0},{1,0},{0,1},{0,-1}};
-    vector<vector<int>>dp;
-    bool isValid(vector<int>pos,vector<vector<int>>& matrix)
-    {
-        return (pos[0]>=0 && pos[1]>=0 && pos[0]<matrix.size() && pos[1]<matrix[0].size());
-    }
-    int f(vector<int>pos,vector<vector<int>>& matrix,vector<vector<int>>& vis)
-    {
-        if(dp[pos[0]][pos[1]]!=-1)return dp[pos[0]][pos[1]];
-        vis[pos[0]][pos[1]]=1;
-        int mx=0;
-        for(auto i:dd)
-        {
-            int x=pos[0]+i[0],y=pos[1]+i[1];
-            if(isValid({x,y},matrix) && !vis[x][y] && matrix[x][y]>matrix[pos[0]][pos[1]])
-                mx=max(mx,1+f({x,y},matrix,vis));
-        }
-        vis[pos[0]][pos[1]]=0;
-        return dp[pos[0]][pos[1]]=mx;
-    }
+    int m, n;
+    int memo[201][201];
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-        int mx=0;
-        dp.resize(matrix.size(),vector<int>(matrix[0].size(),-1));
-        vector<vector<int>>vis(matrix.size(),vector<int>(matrix[0].size(),0));
-        for(int i=0;i<matrix.size();i++)
-        {
-            for(int j=0;j<matrix[0].size();j++)
-            {
-                mx=max(mx,f({i,j},matrix,vis));
-            }
+        m = matrix.size();
+        n = matrix[0].size();
+        int ans = 0;
+        for (int r = 0; r < m; ++r)
+            for (int c = 0; c < n; ++c)
+                ans = max(ans, dfs(matrix, r, c));
+        return ans;
+    }
+    int DIR[5] = {0, 1, 0, -1, 0};
+    int dfs(vector<vector<int>>& matrix, int r, int c) {
+        if (memo[r][c] != 0) return memo[r][c];
+        int ans = 1;
+        for (int i = 0; i < 4; ++i) {
+            int nr = r + DIR[i], nc = c + DIR[i+1];
+            if (nr < 0 || nr == m || nc < 0 || nc == n || matrix[nr][nc] <= matrix[r][c]) continue;
+            ans = max(ans, dfs(matrix, nr, nc) + 1);
         }
-        return mx+1;
+        return memo[r][c] = ans;
     }
 };
