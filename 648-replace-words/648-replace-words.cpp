a5f06cd1d4f03852bdb8f773d03f12c9368a22d2
RAJA SETHI <55877197+RAJASETHI@users.noreply.github.com>
2022-06-23 11:28:11 +0530

    Time: 74 ms (70.19%), Space: 50.8 MB (58.13%) - LeetHub

--- 648-replace-words/648-replace-words.cpp
+++ 648-replace-words/648-replace-words.cpp
@@ -1,26 +1,71 @@
 class Solution {
 public:
-    string replaceWords(vector<string>& dict, string s) {
-        unordered_map<string,int>mp;
-        for(auto i:dict)mp[i]++;
-        stringstream ss(s);
+    string replaceWords(vector<string>& dict, string sentence) {
+        stringstream sen(sentence);
+        string res="";
         string token;
-        string res;
-        while(getline(ss,token,' '))
-        {
-            string tmp;
-            for(auto i:token)
-            {
-                tmp+=i;
-                if(mp[tmp])
-                {
-                    res+=tmp+" ";
+        TrieTree tree;
+        tree.buildTree(dict);
+         while(getline(sen,token,' ')){
+             res+=tree.replace(token);
+             res+=" ";
+         }
+        return res.substr(0,res.size()-1);  //ignore last space
+    }
+    
+class TrieNode{
+public:
+    bool end;
+    string str;
+    TrieNode* children[26];
+    TrieNode(bool flag, string s){
+        end=flag;
+        str=s;
+        memset(children, 0, sizeof(children));;
+        }
+    };
+class TrieTree{
+public:
+        TrieNode* root;
+        TrieTree(){
+            root= new TrieNode(false,"");
+        }
+    
+       void buildTree(vector<string>& dict){
+            for(string s:dict){
+                TrieNode* cur =root;
+                for(char c:s){
+                    if(cur->children[c-'a'] == NULL){
+                        cur->children[c-'a'] = new TrieNode(false,"");
+                        cur = cur->children[c-'a'];
+                    }
+                    else if(cur->children[c-'a']->end==true){   //if a shorter root exists, we just stop build
+                        cur=NULL;
+                        break;
+                    }
+                    else cur=cur->children[c-'a'];
+                }
+                if(cur!=NULL){    //mark the string and flag
+                    cur->end=true;
+                    cur->str=s;
+                }
+            }
+           return ;
+        }
+        string replace(string s){
+            TrieNode* cur =root;
+            string res="";
+            for(char c:s){
+                cur=cur->children[c-'a'];
+                if(cur==NULL) break;
+                if(cur->end==true){
+                    res=cur->str;
                     break;
                 }
-                else if(tmp==token)res+=token+" ";
             }
+            if(res!="") return res;
+            return s;
         }
-        res.pop_back();
-        return res;
-    }
+    };
+
 };
