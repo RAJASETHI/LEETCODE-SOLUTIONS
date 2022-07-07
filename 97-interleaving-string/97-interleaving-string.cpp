f90e33b5bb03c3d23cee8ed5c5649d5ef80c32f0
RAJA SETHI <55877197+RAJASETHI@users.noreply.github.com>
2022-07-07 09:25:17 +0530

    Time: 65 ms (18.82%), Space: 25.4 MB (24.91%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool f(int i,int j,int k,string s2, string s1, string s3,vector<vector<vector<int>>>&dp)
+    bool f(int i,int j,int k,string s2, string s1, string s3,vector<vector<int>>&dp)
     {
         if(i==s1.size())
         {
@@ -22,15 +22,16 @@ public:
             }
             return true;
         }
-        if(dp[i][j][k]!=-1)return dp[i][j][k];
-        if(s1[i]==s2[j] && s2[j]==s3[k])return dp[i][j][k]=(f(i+1,j,k+1,s2,s1,s3,dp) || f(i,j+1,k+1,s2,s1,s3,dp));
-        else if(s1[i]==s3[k])return dp[i][j][k]=f(i+1,j,k+1,s2,s1,s3,dp);
-        else if(s2[j]==s3[k])return dp[i][j][k]=f(i,j+1,k+1,s2,s1,s3,dp);
-        return dp[i][j][k]=false;
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(s1[i]==s2[j] && s2[j]==s3[k])return dp[i][j]=(f(i+1,j,k+1,s2,s1,s3,dp) || f(i,j+1,k+1,s2,s1,s3,dp));
+        else if(s1[i]==s3[k])return dp[i][j]=f(i+1,j,k+1,s2,s1,s3,dp);
+        else if(s2[j]==s3[k])return dp[i][j]=f(i,j+1,k+1,s2,s1,s3,dp);
+        return dp[i][j]=false;
     }
     bool isInterleave(string s2, string s1, string s3) {
         int i=0,j=0,k=0,n=s1.size(),m=s2.size(),l=s3.size();
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(m+1,vector<int>(l+1,-1)));
+        // vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(m+1,vector<int>(l+1,-1)));
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
         if(n+m!=l)return false;
         return f(0,0,0,s2,s1,s3,dp);
     }
