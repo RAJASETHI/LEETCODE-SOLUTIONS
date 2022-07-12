5fc6df032973ebdd64b508df951b252260805cb3
RAJA SETHI <55877197+RAJASETHI@users.noreply.github.com>
2022-07-12 23:07:47 +0530

    Time: 11 ms (94.43%), Space: 10.2 MB (30.06%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -1,39 +1,32 @@
 class Solution {
-public:
-    bool f(int idx,vector<int>sum,int target,vector<int>& mat)
-    {
-        if(idx==mat.size()){
-            for(int i=1;i<4;i++)
-            {
-                if(sum[i]!=sum[i-1])return false;
-            }
-            return true;
+    bool dfs(vector<int> &sidesLength,const vector<int> &matches, int index, const int target) {
+        if (index == matches.size())
+            return sidesLength[0] == sidesLength[1] && sidesLength[1] == sidesLength[2] && sidesLength[2] == sidesLength[3];
+        for (int i = 0; i < 4; ++i) {
+            if (sidesLength[i] + matches[index] > target) // first
+                continue;
+            int j = i;
+            while (--j >= 0) // third
+                if (sidesLength[i] == sidesLength[j]) 
+                    break;
+            if (j != -1) continue;
+            sidesLength[i] += matches[index];
+            if (dfs(sidesLength, matches, index + 1, target))
+                return true;
+            sidesLength[i] -= matches[index];
         }
-        bool ans=0;
-        for(int i=0;i<4;i++)
-        {
-            if(sum[i]>target)return false;
-            int j=i;
-            while(--j>=0)
-            {
-                if(sum[i]==sum[j])break;
-            }
-            if(j!=-1)continue;
-            sum[i]+=mat[idx];
-            ans=ans || f(idx+1,sum,target,mat);
-            if(ans)return true;
-            sum[i]-=mat[idx];
-        }
-        return ans;
+        return false;
     }
-    bool makesquare(vector<int>& matchsticks) {
-        vector<int>sum(4,0);
-        int target=0;
-        for(auto i:matchsticks)target+=i;
-        sort(matchsticks.begin(),matchsticks.end(),greater<int>());
-        if(target%4!=0)return false;
-        target/=4;
-        return f(0,sum,target,matchsticks);
-        
+public:
+    bool makesquare(vector<int>& nums) {
+        if (nums.size() < 4) return false;
+        int sum = 0;
+        for (const int val: nums) {
+            sum += val;
+        }
+        if (sum % 4 != 0) return false;
+        sort(nums.begin(), nums.end(), [](const int &l, const int &r){return l > r;}); // second
+        vector<int> sidesLength(4, 0);
+        return dfs(sidesLength, nums, 0, sum / 4);
     }
 };
