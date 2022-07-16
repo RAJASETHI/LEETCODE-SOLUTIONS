db2c7747c849cc1c56af4b255f0772572d2a3c64
RAJA SETHI <55877197+RAJASETHI@users.noreply.github.com>
2022-07-16 08:59:56 +0530

    Time: 217 ms (5.03%), Space: 30.4 MB (6.12%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     int mod=pow(10,9)+7;
-    vector<vector<vector<int>>>dp;
-    int f(int m, int n, int maxMove, int startRow, int startColumn)
+    vector<vector<int>>dd={{-1,0},{1,0},{0,1},{0,-1}};
+    int f(vector<int>pos,vector<int>&sz,int mxMove,vector<vector<vector<int>>>&dp)
     {
-        if(startRow>=m || startRow<0 || startColumn<0 || startColumn>=n )return 1;
-        if(!maxMove)return 0;
-        if(dp[startRow][startColumn][maxMove]!=-1)return dp[startRow][startColumn][maxMove];
-        return dp[startRow][startColumn][maxMove]=((f(m,n,maxMove-1,startRow+1,startColumn)%mod+f(m,n,maxMove-1,startRow,startColumn+1)%mod)%mod+(f(m,n,maxMove-1,startRow-1,startColumn)%mod+f(m,n,maxMove-1,startRow,startColumn-1)%mod)%mod)%mod;
+        if(pos[0]>=sz[0] || pos[1]>=sz[1] || pos[0]<0 || pos[1]<0)return 1;
+        if(mxMove<=0)return 0;
+        if(dp[pos[0]][pos[1]][mxMove]!=-1)return dp[pos[0]][pos[1]][mxMove];
+        int ans=0;
+        for(auto i:dd)
+        {
+                int t_x=i[0]+pos[0],t_y=i[1]+pos[1];
+                ans=(ans+f({t_x,t_y},sz,mxMove-1,dp))%mod;
+        }
+        return dp[pos[0]][pos[1]][mxMove]=ans;
     }
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        dp.resize(m+1,vector<vector<int>>(n+1,vector<int>(maxMove+1,-1)));
-        return f(m,n,maxMove,startRow,startColumn);
+        vector<vector<vector<int>>>dp(m,vector<vector<int>>(n,vector<int>(maxMove+1,-1)));
+        vector<vector<int>>vis(m,vector<int>(n,0));
+        vector<int>sz={m,n};
+        return f({startRow,startColumn},sz,maxMove,dp);
     }
 };
