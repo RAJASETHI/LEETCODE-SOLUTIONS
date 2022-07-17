b348308392fd8f04c209b6949fe81583d96841a6
RAJA SETHI <55877197+RAJASETHI@users.noreply.github.com>
2022-07-17 10:00:25 +0530

    Time: 1882 ms (11.11%), Space: 518.9 MB (11.11%) - LeetHub

---
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    // struct cmp{
+        static bool cmp(pair<string,int>&p1,pair<string,int>&p2)
+        {
+            if(p1.first==p2.first)return p1.second<p2.second;
+            return p1.first<p2.first;
+        }
+    // };
+    vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
+        vector<int>res;
+        unordered_map<int,vector<pair<string,int>>>mp;
+        for(int i=0;i<=nums[0].size();i++)
+        {
+            int cp=0;
+            for(auto x:nums)
+            {
+                mp[i].push_back({x.substr(x.size()-i),cp});
+                cp++;
+            }
+            
+        }
+        // for(auto &i:mp)
+        // {
+        //     sort(i.second.begin(),i.second.end(),cmp);
+        // }
+        for(auto i:queries)
+        {
+            sort(mp[i[1]].begin(),mp[i[1]].end(),cmp);
+            res.push_back(mp[i[1]][i[0]-1].second);
+        }
+        return res;
+    }
+};
