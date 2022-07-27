cf8cf41f4589d52c00d64dbbd8a3d6668f550e4d
RAJA SETHI <55877197+RAJASETHI@users.noreply.github.com>
2022-07-27 19:02:45 +0530

    Time: 353 ms (61.78%), Space: 132.3 MB (74.56%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
-    vector<int>dp;
-    int f(int idx,vector<int>&stone)
-    {
-        if(idx>=stone.size())return 0;
-        int sm=0,mn=INT_MIN;
-        if(dp[idx]!=-1)return dp[idx];
-        for(int i=idx;i<min(idx+3,(int)stone.size());i++)
-        {
-            sm+=stone[i];
-            mn=max(mn,sm-f(i+1,stone));
-        }
-        return dp[idx]=mn;
-    }
-    string stoneGameIII(vector<int>& stoneValue) {
+//     vector<int>dp;
+//     int f(int idx,vector<int>&stone)
+//     {
+//         if(idx>=stone.size())return 0;
+//         int sm=0,mn=INT_MIN;
+//         if(dp[idx]!=-1)return dp[idx];
+        
+//         return dp[idx]=mn;
+//     }
+    // string stoneGameIII(vector<int>& stoneValue) {
+    //     string res="Tie";
+    //     int n=stoneValue.size();
+    //     dp.resize(n,-1);
+    //     int val=f(0,stoneValue);
+    //     if(val>0)res="Alice";
+    //     else if(val<0)res="Bob";
+    //     return res;
+    // }
+    string stoneGameIII(vector<int>& stone) {
         string res="Tie";
-        int n=stoneValue.size();
-        dp.resize(n,-1);
-        int val=f(0,stoneValue);
+        int n=stone.size();
+        vector<int>dp(n+1,0);
+        for(int idx=n-1;idx>=0;idx--)
+        {
+            int sm=0,mn=INT_MIN;
+            for(int i=idx;i<min(idx+3,(int)stone.size());i++)
+            {
+                sm+=stone[i];
+                mn=max(mn,sm-dp[i+1]);
+            }
+            dp[idx]=mn;
+        }
+        int val=dp[0];
         if(val>0)res="Alice";
         else if(val<0)res="Bob";
         return res;
