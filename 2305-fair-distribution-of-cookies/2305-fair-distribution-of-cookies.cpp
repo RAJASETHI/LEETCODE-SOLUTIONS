3b08897cc0b51e67baec0c6b565cab912eaaf855
RAJA SETHI <55877197+RAJASETHI@users.noreply.github.com>
2022-07-28 17:32:35 +0530

    Time: 2741 ms (5.00%), Space: 7 MB (25.23%) - LeetHub

---
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int mx=INT_MAX;
+    void f(int idx,vector<int>&nums,vector<int>&cookies)
+    {
+        if(idx==cookies.size())
+        {
+            mx=min(mx,*max_element(nums.begin(),nums.end()));
+            return;
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            nums[i]+=cookies[idx];
+            f(idx+1,nums,cookies);
+            nums[i]-=cookies[idx];
+        }
+        
+    }
+    int distributeCookies(vector<int>& cookies, int k) {
+        mx=INT_MAX;
+        vector<int>nums(k,0);
+        f(0,nums,cookies);
+        return mx;
+    }
+};
