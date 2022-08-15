54c8991c4b7a297f30886799a191686e26383ed3
RAJA SETHI <55877197+RAJASETHI@users.noreply.github.com>
2022-08-15 12:48:46 +0530

    Time: 75 ms (85.24%), Space: 18.1 MB (39.60%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    int maximalSquare(vector<vector<char>>& mat) {
-        int m=mat.size(),n=mat[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,0));
-        int mx=0;
-        for(int i=0;i<m;i++)
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int n=matrix.size(),m=matrix[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        int mxSqr=0;
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<m;j++)
             {
-                if(!i || !j)
+                if(matrix[i][j]-'0')
                 {
-                    dp[i][j]=mat[i][j]-'0';
+                    if(!i || !j)
+                    {
+                        dp[i][j]=1;
+                    }
+                    else
+                    {
+                        dp[i][j]=min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]})+1;
+                    }
+                    mxSqr=max(mxSqr,dp[i][j]);
                 }
-                else
-                {
-                    if(mat[i][j]-'0')
-                        dp[i][j]=min({dp[i-1][j],dp[i-1][j-1],dp[i][j-1]})+1;
-                    
-                }
-                mx=max(mx,dp[i][j]);
             }
         }
-        return mx*mx;
+        return mxSqr*mxSqr;
+        
     }
 };
